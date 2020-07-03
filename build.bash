2f6bec3cd97a0f438062f08260f4596108a77ed3
Alexander A. Klimov <alexander.klimov@icinga.com>
2020-07-03 16:32:08 +0200

    Add build.bash

---
+++ build.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -exo pipefail
+
+IDBSRC="$1"
+
+if [ -z "$IDBSRC" ]; then
+	cat <<EOF >&2
+Usage: ${0} /icingadb/source/dir
+EOF
+
+	false
+fi
+
+IDBSRC="$(realpath "$IDBSRC")"
+BLDCTX="$(realpath "$(dirname "$0")")"
+
+docker build -f "${BLDCTX}/action-base.Dockerfile" -t icinga/icingadb-builder "$BLDCTX"
+docker build -f "${BLDCTX}/deps.Dockerfile" -t icinga/icingadb-deps "$BLDCTX"
+
+docker run --rm -i \
+	-v "${IDBSRC}:/idbsrc:ro" \
+	-v "${BLDCTX}:/bldctx:ro" \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	icinga/icingadb-builder bash <<EOF
+set -exo pipefail
+
+git -C /idbsrc archive --prefix=idbcp/ HEAD |tar -xC /
+cd /idbcp
+
+CGO_ENABLED=0 go build -ldflags '-s -w' -o icingadb .
+upx icingadb
+
+docker build -f /bldctx/Dockerfile -t icinga/icingadb .
+EOF
